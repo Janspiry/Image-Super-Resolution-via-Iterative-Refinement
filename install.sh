19f109224057601e9931e5e6173e6eb2ba8b22a8
Piper Wolters <piperw@allenai.org>
2023-05-05 18:53:26 +0000

    more env

--- install.sh
+++ install.sh
@@ -2,6 +2,5 @@
 
 apt-get update && apt-get install ffmpeg libsm6 libxext6  -y 
 apt-get update && apt-get install -y libgl1-mesa-dev
-apt-get update && apt-get install -y libxxf86vm1
 
 pip install -r requirements.txt
