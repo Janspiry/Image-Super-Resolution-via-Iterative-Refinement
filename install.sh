0fbfa7290fad030b1d0d2b994b29c59ba17a614e
Piper Wolters <piperw@allenai.org>
2023-05-05 22:01:16 +0000

    bug

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-RUN apt-get update && apt-get install -y libgl1-mesa-dev
-RUN apt-get update && apt-get install -y libxxf86vm1
+apt-get update && apt-get install -y libgl1-mesa-dev
+apt-get update && apt-get install -y libxxf86vm1
 
 pip install -r requirements.txt
