e20992ace36734eea051c604af83d55bb9406f6c
Piper Wolters <piperw@allenai.org>
2023-05-05 22:09:51 +0000

    :(

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 apt-get update && apt-get install -y libgl1-mesa-dev
 apt-get update && apt-get install -y libxxf86vm1
+apt-get update && apt-get install ffmpeg libsm6 libxext6  -y
 
+pip install opencv-python
 pip install -r requirements.txt
