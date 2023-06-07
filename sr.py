112abe5988a381c4bfee9aadcf2a0b04cadabd0e
Piper Wolters <piperwolters@gmail.com>
2023-06-07 15:39:59 -0700

    start of the aggregate 3d model and double checking preemptive code again

--- sr.py
+++ sr.py
@@ -22,6 +22,7 @@ if __name__ == "__main__":
     parser.add_argument('-enable_wandb', action='store_true')
     parser.add_argument('-log_wandb_ckpt', action='store_true')
     parser.add_argument('-log_eval', action='store_true')
+    parser.add_argument('-auto_resume', action='store_true')
     
     # parse configs
     args = parser.parse_args()
@@ -67,6 +68,25 @@ if __name__ == "__main__":
                 val_set, dataset_opt, phase)
     logger.info('Initial Dataset Finished')
 
+    # Manually check for the most recent checkpoint in this results dir.
+    # If auto_resume is not specified, code will load in the resume_state arg if provided,
+    # otherwise will not load any weight in (not even the last_opt.th and last_gen.pth).
+    if args.auto_resume:
+        chkpt_path = opt['path']['checkpoint']
+        best_weight, best_iters = None, 0
+        for chkpt in os.listdir(chkpt_path):
+            # Just gonna look for opt weights so we don't repeatedly look.
+            if chkpt.endswith('gen.pth'):
+                continue
+            split = chkpt.split('_')
+            iterations = int(split[0][1:])
+            epochs = int(split[1][1:])
+
+            if iterations > best_iters:
+                best_weight = chkpt
+                best_iters = iterations
+        opt['path']['resume_state'] = os.path.join(opt['path']['checkpoint'], best_weight[:-8])
+
     # model
     diffusion = Model.create_model(opt)
     logger.info('Initial Model Finished')
@@ -75,18 +95,6 @@ if __name__ == "__main__":
     current_step = diffusion.begin_step
     current_epoch = diffusion.begin_epoch
     n_iter = opt['train']['n_iter']
-
-    # FOR BEAKER: check if there is an existing "last" checkpoint within this experiment results dir.
-    last_gen_check = os.path.join(opt['path']['checkpoint'], 'last_gen.pth')
-    last_opt_check = os.path.join(opt['path']['checkpoint'], 'last_opt.pth')
-    if os.path.exists(last_gen_check) and os.path.exists(last_opt_check):
-        print("Resuming from last checkpoints...", last_gen_check, " and ", last_opt_check)
-        opt['path']['resume_state'] = os.path.join(opt['path']['checkpoint'], 'last')
-
-    # If not resuming from last checkpoint and just trying to load in weights, it should default to here.
-    elif (opt['path']['resume_gen_state'] and opt['path']['resume_opt_state']) or opt['path']['resume_state']:
-        logger.info('Resuming training from epoch: {}, iter: {}.'.format(
-            current_epoch, current_step))
 
     # Attempting to have both the original scheduling option (DDPM) and the huggingface DDIM implementation here.
     diffusion.set_new_noise_schedule(
